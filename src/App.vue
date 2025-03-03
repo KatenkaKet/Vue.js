46197f03533dbd2721683fb35bab7f520c10864b
KatenkaKet <katenkeket13@gmail.com>
2025-03-03 13:03:34 +0500

    PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li><router-link to="/">Главная</router-link></li>
-        <li><router-link to="/corpuses">Корпусы</router-link></li>
-        <li><router-link to="/room">Комнаты</router-link></li>
-      </ul>
-      <div v-if="isAuthunticated && user">
-        Wellcome, {{user.name}}
-        <button @click="logout">Logout</button>
+  <Menubar :model="items">
+    <template #start>
+      <span>
+        <img src="@/assets/logo.svg" alt="logo" width="50" />
+      </span>
+    </template>
+    <template #item="{ item, props, hasSubmenu, root }">
+      <router-link v-if="item.route" :to="item.route" class="flex items-center ml-6 p-4">
+        <span :class="item.icon"/>
+        <span class="ml-1">{{item.label}}</span>
+      </router-link>
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2">
+        <div v-if="isAuthenticated && user">
+          <span class="pi pi-fw pi-user mr-4"/>{{user.name}}
+          <Button @click="logout" class="ml-4 bg-[#FDE94C] hover:bg-[#f0d535]">Logout</Button>
+        </div>
+        <div v-else>
+          <form @submit.prevent="login">
+            <InputText v-model="email" type="email" id="email" required
+                       placeholder="Email" class="m-2 sm:w-auto" />
+            <InputText v-model="password" type="password" id="password" required
+                       placeholder="password" class="m-2 sm:w-auto"  :class="{'p-invalid': authError}"/>
+            <Button type="submit">Login</Button>
+            <div class="ml-2">
+              <small v-if="authError" class="error">{{authError}}</small>
+            </div>
+          </form>
+        </div>
       </div>
-      <div v-else>
-        <form @submit.prevent="login">
-          <div>
-            <label for="email">Email:</label>
-            <input v-model="email" type="email" id="email" required />
-          </div>
-          <div>
-            <label for="password">Password:</label>
-            <input v-model="password" type="password" id="password" required />
-          </div>
-          <button type="submit">Login</button>
-          <p v-if="authError" class="error">{{authError}}</p>
-        </form>
-      </div>
-    </nav>
-  </header>
+    </template>
+  </Menubar>
   <router-view></router-view>
 </template>
 
 <script>
 import {useAuthStore} from "@/stores/authStore.js";
+import Button from "primevue/button";
+import Menubar from "primevue/menubar";
+import InputText from "primevue/inputtext";
 export default {
+  components: {Button, Menubar, InputText},
   data(){
     return{
       email:"",
       password:"",
-      authStore: useAuthStore()
+      authStore: useAuthStore(),
+      items: [
+        {
+          label: "Главная",
+          route: "/",
+          icon: "pi pi-fw pi-home",
+        },
+        {
+          label: "Корпусы",
+          route: "/corpuses",
+          icon: "pi pi-fw pi-building",
+        },
+        {
+          label: "Комнаты",
+          route: "/room",
+          icon: "pi pi-fw pi-table"
+        }
+      ]
     };
   },
   computed:{
-    isAuthunticated(){
-      return this.authStore.isAuthunticated;
+    isAuthenticated(){
+      return this.authStore.isAuthenticated;
     },
     user(){
       return this.authStore.user;
@@ -61,7 +88,7 @@ export default {
   mounted(){
     const token = localStorage.getItem("token");
     if(token){
-      this.authStore.isAuthunticated = true;
+      this.authStore.isAuthenticated = true;
       this.authStore.getUser();
     }
   }
@@ -72,4 +99,16 @@ export default {
 .error{
   color: red;
 }
+
+.p-button:hover {
+  background-color: #fdc54c !important;
+  border-color: #fdc54c !important;
+}
+
+.p-button{
+  background-color: #fde94c;
+  border-color: #fde94c;
+  color: #992d2f;
+}
+
 </style>
